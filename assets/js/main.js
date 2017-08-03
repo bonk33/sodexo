0b2b231c91a9014d5f8dc3f55c3cfa6af7187dc9
Daniel Addae <nanaayimda@gmail.com>
2017-08-03 20:01:03 +0100

    added nav login

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var navLogin = document.getElementById('nav-login');
+var navLoginForm = document.getElementById('nav-login-form');
+
+navLogin.addEventListener("click", function(event) {
+    navLoginForm.classList.toggle('hidden');
+});
